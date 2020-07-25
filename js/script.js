7a134bf9852d0366fe7677367a17308e10ed1197
Laura Abowd <laura.abowd@gmail.com>
2020-07-25 13:30:00 -0500

    choose random category function

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -99,6 +99,9 @@ function buildAndShowHomeHTML (categories) {
     function (homeHtml) {
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
+      var chosenCategoryShortName = chooseRandomCategory(categories);
+
+
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
